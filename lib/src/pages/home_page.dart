1cb79c0930fb0b115ffa6fbd517f774d4d1807d9
hijuliancode <hijuliansosa@protonmail.com>
2020-08-19 22:40:30 -0500

    Columnas y Estilos

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 
 class HomePage extends StatelessWidget {
-  
+
+  final estiloTexto = TextStyle(fontSize: 26);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +13,12 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Text('Hola Homepage'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Número de clicks', style: estiloTexto),
+            Text('0', style: estiloTexto)
+        ],),
       )
     );
   }

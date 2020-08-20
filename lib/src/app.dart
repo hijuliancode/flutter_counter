d7b18e63ab9f2bb0e231a84f1d91bd3441397a66
hijuliancode <hijuliansosa@protonmail.com>
2020-08-19 22:13:09 -0500

    Widget - Scaffold

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_couter/src/pages/home_page.dart';
+
 class MyApp extends StatelessWidget {
 
   @override
   Widget build( context ) {
     return MaterialApp (
       home: Center(
-        child: Text('Hola Mundo!'),
+        child: HomePage(),
       )
     );
   }
